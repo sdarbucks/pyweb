81edbdd7e60b1a8dcdb35e49af83fb57fb3138d3
sdarbucks <kdh797979@naver.com>
2021-10-14 12:41:29 +0900

    signup, modify

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,7 +8,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     # 127.0.0.1:8000/pybo/1/
     path('<int:question_id>/', views.detail, name='detail'),
-    # 127.0.0.1:8000/question/create/
+    # 127.0.0.1:8000/question/create/   질문 등록
     path('question/create/', views.question_create, name='question_create'),
-    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create')
+    # 답변 등록
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    # 질문 수정
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.utils import timezone
@@ -19,12 +20,14 @@ def detail(request, question_id):
     return render(request, 'pybo/detail.html', context)
 
 # 질문 등록
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method =='POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)  # 임시 저장
             question.create_date = timezone.now()   # 등록일
+            question.author = request.user  # 세션권한이 있는 user(author)
             question.save() # 실제 저장
             return redirect('pybo:index')
     else:   # request.method =='GET'
@@ -33,12 +36,14 @@ def question_create(request):
     return render(request, 'pybo/question_form.html', context)
 
 # 답변 등록
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = Question.objects.get(id=question_id) # 질문 1개 가져오기
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question  # 질문을 답변에 저장(외래키)
             form.save()
@@ -47,3 +52,20 @@ def answer_create(request, question_id):
         form  = AnswerForm()
     context = {'form': form}
     return render(request, 'pybo:detail', context)
+
+# 질문 수정
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = Question.objects.get(id=question_id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # 세션권한이 있는 user 생성
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+    else:
+        form = QuestionForm(instance=question)  # 기존의 내용을 가져옴(question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
