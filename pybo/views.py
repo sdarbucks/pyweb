6e8d189f2ce79c9b219a12e61ddc95581f4e7ae1
sdarbucks <kdh797979@naver.com>
2021-10-15 13:16:30 +0900

    update

--- pybo/views.py
+++ pybo/views.py
@@ -1,21 +1,34 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
 from pybo.models import Question
 from pybo.forms import QuestionForm, AnswerForm
 
-# 전체 목록 조회
 def index(request):
-    # return HttpResponse("Welcome~ MySite!!")
-    question_list = Question.objects.all()
-    context = {'question_list':question_list}
+    return render(request, 'pybo/index.html')
+
+# 전체 목록 조회
+def board(request):
+    
+    # 페이지
+    page = request.GET.get('page', '1')
+    
+    # 조회
+    question_list = Question.objects.order_by('-create_date')   # 내림차순 정렬
+
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 # 상세 페이지 조회
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/detail.html', context)
 
@@ -29,7 +42,7 @@ def question_create(request):
             question.create_date = timezone.now()   # 등록일
             question.author = request.user  # 세션권한이 있는 user(author)
             question.save() # 실제 저장
-            return redirect('pybo:index')
+            return redirect('pybo:board')
     else:   # request.method =='GET'
         form = QuestionForm()
     context = {'form': form}
@@ -38,7 +51,8 @@ def question_create(request):
 # 답변 등록
 @login_required(login_url='common:login')
 def answer_create(request, question_id):
-    question = Question.objects.get(id=question_id) # 질문 1개 가져오기
+    # question = Question.objects.get(id=question_id) # 질문 1개 가져오기
+    question = get_object_or_404(Question, pk=question_id)
     if request.method == 'POST':
         form = AnswerForm(request.POST)
         if form.is_valid():
@@ -56,7 +70,8 @@ def answer_create(request, question_id):
 # 질문 수정
 @login_required(login_url='common:login')
 def question_modify(request, question_id):
-    question = Question.objects.get(id=question_id)
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = QuestionForm(request.POST, instance=question)
         if form.is_valid():
@@ -69,3 +84,11 @@ def question_modify(request, question_id):
         form = QuestionForm(instance=question)  # 기존의 내용을 가져옴(question)
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+# 질문 삭제
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    question.delete()
+    return redirect('pybo:index')
